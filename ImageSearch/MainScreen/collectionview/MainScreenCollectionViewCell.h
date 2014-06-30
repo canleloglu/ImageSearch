2d33c354f7ef273f2bb30e23b9239c5d6089a523
Can Leloglu <canleloglu@sabanciuniv.edu>
2014-06-29 22:10:37 -0700

    deleted the framework imported all the sdwebimage source files - works with warnings

--- ImageSearch/MainScreen/collectionview/MainScreenCollectionViewCell.h
+++ ImageSearch/MainScreen/collectionview/MainScreenCollectionViewCell.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <SDWebImage/UIImageView+WebCache.h>
+#import "UIImageView+WebCache.h"
 
 @interface MainScreenCollectionViewCell : UICollectionViewCell
 
--- ImageSearch/imagedetail/ImageDetailView.h
+++ ImageSearch/imagedetail/ImageDetailView.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <SDWebImage/UIImageView+WebCache.h>
+#import "UIImageView+WebCache.h"
 #import "ActivityView.h"
 
 @protocol ImageDetailViewDelegate <NSObject>
